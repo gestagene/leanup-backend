e10011ba24788c6cbf0ea751be0b125401f7218d
gestagene <gestagenebrian@gmail.com>
2026-05-23 17:28:29 +0800

    feat: add support for ES256

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,19 +1,30 @@
 from fastapi import HTTPException, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jose import jwt, JWTError
-from app.core.config import SUPABASE_JWT_SECRET
+from app.core.config import SUPABASE_JWT_SECRET, SUPABASE_URL
+import httpx
+import os
 
 security = HTTPBearer()
+
+def get_jwks():
+    url = f"{SUPABASE_URL}/auth/v1/.well-known/jwks.json"
+    response = httpx.get(url)
+    return response.json()
 
 def get_current_user(credentials: HTTPAuthorizationCredentials = Security(security)):
     token = credentials.credentials
     try:
+        jwks = get_jwks()
+        keys = jwks.get("keys", [])
+        
         payload = jwt.decode(
             token,
-            SUPABASE_JWT_SECRET,
-            algorithms=["HS256"],
+            keys,
+            algorithms=["ES256", "HS256"],
             audience="authenticated"
         )
         return payload
-    except JWTError:
+    except JWTError as e:
+        print("JWT ERROR:", str(e))
         raise HTTPException(status_code=401, detail="Invalid or expired token")
